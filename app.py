5a75d95bbb85a63e0ef38aebac271be129352147
Ailer20 <aronvieira820@gmail.com>
2025-10-14 16:44:22 -0400

    v2

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
@@ -25,3 +24,4 @@ def handle_start_game_request():
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+
